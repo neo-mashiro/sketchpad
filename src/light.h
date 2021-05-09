bc5a295e6a42615b4cd843e98b3d5af86153b5ba
neo-mashiro <neo-mashiro@hotmail.com>
2021-05-09 02:24:34 -0400

    remove unused headers

---
+++ src/light.h
@@ -0,0 +1,15 @@
+// #include <ctime>
+//
+// #include <glm/glm.hpp>
+// #include <glm/gtc/type_ptr.hpp>
+// #include <glm/gtc/matrix_transform.hpp>
+// #include <glm/gtx/transform.hpp>
+// #include <glm/gtx/perpendicular.hpp>
+//
+// struct Textures {
+//    GLuint base{ 0 };
+//    GLuint normal{ 0 };
+//    GLuint height{ 0 };
+//    GLuint occlusion{ 0 };
+//    GLuint roughness{ 0 };
+// };
