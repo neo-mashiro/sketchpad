9b1191ecc4e2ae4e3d64a37a0bc42ae937570cde
neo-mashiro <neo-mashiro@hotmail.com>
2021-07-31 01:33:27 -0400

    rewrite camera class as derived transform

--- src/scene/camera.cpp
+++ src/scene/camera.cpp
@@ -1,5 +1,16 @@
 #include "pch.h"
 
+#define GLM_ENABLE_EXPERIMENTAL
+
+#pragma warning(push)
+#pragma warning(disable : 4201)
+#include <glm/glm.hpp>
+#include <glm/gtc/type_ptr.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtx/transform.hpp>
+#include <glm/gtx/perpendicular.hpp>
+#pragma warning(pop)
+
 #include "core/input.h"
 #include "core/clock.h"
 #include "core/window.h"
@@ -9,57 +20,8 @@ using namespace core;
 
 namespace scene {
 
-    void Camera::Spin(int delta_x, int delta_y) {
-        if (delta_x == 0 && delta_y == 0) {
-            return;
-        }
-
-        euler_y += delta_x * sensitivity;
-        euler_x += delta_y * sensitivity;
-        euler_x = glm::clamp(euler_x, -88.0f, 88.0f);  // clamp vertical rotation
-
-        forward = glm::normalize(
-            glm::vec3(
-                cos(glm::radians(euler_y)) * cos(glm::radians(euler_x)),
-                sin(glm::radians(euler_x)),
-                sin(glm::radians(euler_y)) * cos(glm::radians(euler_x))
-            )
-        );
-
-        right = glm::normalize(glm::cross(forward, glm::vec3(0.0f, 1.0f, 0.0f)));
-        up = glm::normalize(glm::cross(right, forward));
-    }
-
-    void Camera::Zoom(float zoom) {
-        fov += zoom * zoom_speed;
-        fov = glm::clamp(fov, 1.0f, 90.0f);
-    }
-
-    void Camera::Move(Direction direction, float deltatime, bool snap) {
-        float elevation = position.y;
-
-        switch (direction) {
-            case Direction::F: position += forward * (move_speed * deltatime); break;
-            case Direction::B: position -= forward * (move_speed * deltatime); break;
-            case Direction::L: position -= right * (move_speed * deltatime); break;
-            case Direction::R: position += right * (move_speed * deltatime); break;
-            case Direction::U: position.y += move_speed * deltatime; break;
-            case Direction::D: position.y -= move_speed * deltatime; break;
-        }
-
-        if (snap) {
-            position.y = elevation;  // snap to the plane
-        }
-    }
-
-    Camera::Camera(glm::vec3 position, glm::vec3 forward, float euler_x, float euler_y)
-        : position{position}, forward{forward}, euler_x(euler_x), euler_y(euler_y),
-          fov(90.0f), near_clip(0.1f), far_clip(100.0f),
-          move_speed(1.8f), zoom_speed(1.0f), sensitivity(0.2f) {
-
-        right = glm::normalize(glm::cross(forward, glm::vec3(0.0f, 1.0f, 0.0f)));
-        up = glm::normalize(glm::cross(right, forward));
-    }
+    Camera::Camera()
+        : fov(90.0f), near_clip(0.1f), far_clip(100.0f), move_speed(3.0f), rotate_speed(0.3f) {}
 
     glm::mat4 Camera::GetViewMatrix() const {
         return glm::lookAt(position, position + forward, up);
@@ -69,34 +31,43 @@ namespace scene {
         return glm::perspective(glm::radians(fov), Window::aspect_ratio, near_clip, far_clip);
     }
 
-    void Camera::Update(bool snap) {
-        Spin(Input::ReadMouseAxis(Axis::X), Input::ReadMouseAxis(Axis::Y));
-        Zoom(Input::ReadMouseZoom());
+    void Camera::Update() {
+        // rotation (based on our euler angles convention we need to invert the axis)
+        float euler_y = rotation.y - Input::GetMouseAxis(Axis::Horizontal) * rotate_speed;
+        float euler_x = rotation.x - Input::GetMouseAxis(Axis::Vertical) * rotate_speed;
+        euler_x = glm::clamp(euler_x, -88.0f, 88.0f);  // clamp vertical rotation
 
+        Rotate(glm::vec3(euler_x, euler_y, 0.0f));
+
+        // camera zoom
+        fov += Input::GetMouseZoom();
+        fov = glm::clamp(fov, 1.0f, 90.0f);
+
+        // translation
         float deltatime = Clock::delta_time;
 
-        if (Input::IsKeyPressed('w') || Input::IsKeyPressed(GLUT_KEY_UP)) {
-            Move(Direction::F, deltatime, snap);
+        if (Input::IsKeyPressed('w')) {
+            Translate(forward * (move_speed * deltatime));
         }
 
-        if (Input::IsKeyPressed('s') || Input::IsKeyPressed(GLUT_KEY_DOWN)) {
-            Move(Direction::B, deltatime, snap);
+        if (Input::IsKeyPressed('s')) {
+            Translate(-forward * (move_speed * deltatime));
         }
 
-        if (Input::IsKeyPressed('a') || Input::IsKeyPressed(GLUT_KEY_LEFT)) {
-            Move(Direction::L, deltatime, snap);
+        if (Input::IsKeyPressed('a')) {
+            Translate(-right * (move_speed * deltatime));
         }
 
-        if (Input::IsKeyPressed('d') || Input::IsKeyPressed(GLUT_KEY_RIGHT)) {
-            Move(Direction::R, deltatime, snap);
-        }
-
-        if (Input::IsKeyPressed(VK_SPACE)) {
-            Move(Direction::U, deltatime, snap);
+        if (Input::IsKeyPressed('d')) {
+            Translate(right * (move_speed * deltatime));
         }
 
         if (Input::IsKeyPressed('z')) {
-            Move(Direction::D, deltatime, snap);
+            Translate(-up * (move_speed * deltatime));
+        }
+
+        if (Input::IsKeyPressed(VK_SPACE)) {
+            Translate(up * (move_speed * deltatime));
         }
     }
 }
--- src/scene/camera.h
+++ src/scene/camera.h
@@ -1,46 +1,23 @@
 #pragma once
 
-#pragma warning(push)
-#pragma warning(disable : 4201)
-#include <glm/glm.hpp>
-#include <glm/gtc/type_ptr.hpp>
-#include <glm/gtc/matrix_transform.hpp>
-#include <glm/gtx/transform.hpp>
-#include <glm/gtx/perpendicular.hpp>
-#pragma warning(pop)
+#include "scene/transform.h"
 
 namespace scene {
 
-    enum class Direction { F, B, L, R, U, D };
-
-    class Camera {
-      private:
-        float euler_x;       // euler angles around the x-axis
-        float euler_y;       // euler angles around the y-axis
-
-        void Spin(int delta_x, int delta_y);
-        void Zoom(float zoom);
-        void Move(Direction direction, float deltatime, bool snap);
-
+    class Camera : public Transform {
       public:
-        float fov;           // vertical field of view (fovy)
+        float fov;           // field of view (vertical)
         float near_clip;     // near clipping distance
         float far_clip;      // far clipping distance
-        float zoom_speed;    // scrollwheel zooms in/out the FoV
-        float move_speed;    // keypress translates the camera
-        float sensitivity;   // mouse movement rotates the camera
+        float move_speed;
+        float rotate_speed;
 
-        glm::vec3 position;  // camera position in world space
-        glm::vec3 forward;   // forward direction in world space
-        glm::vec3 right;     // right direction in world space
-        glm::vec3 up;        // up direction in world space
-
-        Camera(glm::vec3 position = glm::vec3(0.0f, 0.0f, 3.0f), glm::vec3 forward = glm::vec3(0.0f, 0.0f, -1.0f),
-            float euler_x = 0.0f, float euler_y = -90.0f);
+        Camera();
+        ~Camera() {}
 
         glm::mat4 GetViewMatrix(void) const;
-        glm::mat4 GetProjectionMatrix() const;
+        glm::mat4 GetProjectionMatrix(void) const;
 
-        void Update(bool snap = false);
+        void Update(void);
     };
 }
